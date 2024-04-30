b61b4b6b19b45e8ba6495bf236904634cb30c61a
ishayankoo001 <ishayankoo@icloud.com>
2024-05-01 00:25:13 +0200

    Added Main.java for test purposes.

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Universe universe = new Universe(new Player[0], 3);
+        Player player1 = new Player("Player1", universe);
+
+        Player player2 = new Player("Player2", universe);
+        Player player3 = new Player("Player3", universe);
+        AcquaintanceArray acquaintanceArray = AcquaintanceArray.createAcquaintanceArray("{(Player1, 1), (Player2, 2), (Player3, 3)}", universe);
+        player1.setAcquaintances(acquaintanceArray);
+        player1.showAcquaintances();
+    }
+}
